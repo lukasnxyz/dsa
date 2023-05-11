aef1d17f0899a9b86730e887489e96e12d3075fe
systemb4 <lukasnitzsche@yahoo.com>
2023-05-10 20:06:39 -0500

    start hashtable.c

---
+++ data_structures/c/hashtable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * hash(k, m) - m is size of hash table
+ * add(key, value) - if key already exists, update value
+ * exists(key)
+ * get(key)
+ * remove(key)
+ */
+
+int main() {
+    return 0;
+}
