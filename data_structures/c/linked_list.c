791033b243ca6672e8ce391963be45029702668c
systemb4 <lukasnitzsche@yahoo.com>
2023-05-12 11:02:48 -0500

    finished linked list implementation

--- data_structures/c/linked_list.c
+++ data_structures/c/linked_list.c
@@ -10,12 +10,10 @@ int size(Node *);
 void printList(Node *);
 void insertFront(Node **, char);
 void insertEnd(Node **, char);
-void insert(Node *, int, char); /* todo */
-char valueAt(Node *, int); /* todo */
-void erase(Node *, int); /* todo */
-
-Node *reverse(Node *); /* todo */
-void eraseValue(Node *, char); /* todo */
+void insert(Node **, int, char);
+char valueAt(Node *, int);
+void erase(Node *, int);
+Node *reverse(Node *);
 
 int size(Node *head) {
     Node *tmp = head;
@@ -65,15 +63,83 @@ void insertEnd(Node **head, char i_value) {
     tmp->next = newNode;
 }
 
+void insert(Node **head, int pos, char value) {
+    if(pos < 1 || pos > size(*head)) {
+        printf("Position is out of bounds!");
+        return;
+    }
+
+    Node *newNode = malloc(sizeof(Node));
+    Node *tmp = *head;
+
+    for(int i = 0; i < pos - 1; i++) {
+        tmp = tmp->next;
+    }
+
+    newNode->value = value;
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+
+char valueAt(Node *head, int pos) {
+    if(pos < 1 || pos > size(head)) {
+        printf("Position is out of bounds!");
+        return ' ';
+    }
+
+    Node *tmp = head;
+    char value;
+
+    for(int i = 0; i < pos - 1; i++) {
+        tmp = tmp->next;
+    }
+
+    value = tmp->value;
+
+    return value;
+}
+
+void erase(Node *head, int pos) {
+    if(pos < 1 || pos > size(head)) {
+        printf("Position is out of bounds!");
+        return;
+    }
+
+    Node *tmp = head;
+
+    for(int i = 0; i < pos - 2; i++) {
+        tmp = tmp->next;
+    }
+
+    Node *tmpOne = tmp->next;
+    tmp->next = tmpOne->next;
+}
+
+Node *reverse(Node *head) {
+    Node *prev = NULL;
+    //Node *curr = head;
+    Node *next = NULL;
+
+    while(head != NULL) {
+        next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    head = prev;
+
+    return head;
+}
+
 int main() {
     Node *head = NULL;
-    insertFront(&head, 'a');
-    insertEnd(&head, 'b');
+    insertFront(&head, 'b');
     insertEnd(&head, 'c');
+    insertEnd(&head, 'd');
     insertFront(&head, 'a');
 
-    printList(head);
-    printf("%d\n", size(head));
+    Node *reversed = reverse(head);
+    printList(reversed);
 
     free(head);
 
